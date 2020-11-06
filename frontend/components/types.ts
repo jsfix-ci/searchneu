5c82e7c648b75ad38d8c92735f359d072e8d1e94
Emily Pillay <epillay221@gmail.com>
2020-11-05 20:55:03 -0500

    EP added in spring 2021

--- frontend/components/types.ts
+++ frontend/components/types.ts
@@ -49,6 +49,10 @@ export enum DayOfWeek {
 export const termDropdownOptions: DropdownItemProps[] =
 [
   {
+    text: 'Spring 2021',
+    value: '202130',
+  },
+  {
     text: 'Fall 2020',
     value: '202110',
   },
