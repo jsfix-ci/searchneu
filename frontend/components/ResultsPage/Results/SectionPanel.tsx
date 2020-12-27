70036839f6ef368ce78028f4c1b98cfac5b01778
Arun Jeevanantham <itsmearun98@gmail.com>
2020-12-27 13:40:41 -0500

    fix keys import

--- frontend/components/ResultsPage/Results/SectionPanel.tsx
+++ frontend/components/ResultsPage/Results/SectionPanel.tsx
@@ -3,7 +3,7 @@ import WeekdayBoxes from './WeekdayBoxes';
 import NotifCheckBox from '../../panels/NotifCheckBox';
 import useSectionPanelDetail from './useSectionPanelDetail';
 import { Meeting, MomentTuple, Section, DayOfWeek } from '../../types';
-import Keys from '../../../../common/keys';
+import Keys from '../../../../common/Keys';
 import IconGlobe from '../../images/IconGlobe';
 
 interface SectionPanelProps {
