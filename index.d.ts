0bcce60f02524efff56e8a52e2ec89e66b368f06
Da-Jin Chu <dajinchu@gmail.com>
2020-12-27 15:11:04 -0500

    use webpack to load svg to prevent caching

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,7 @@
-declare module '*.png'
-declare module '*.jpg'
-declare module '*.jpeg'
-declare module '*.svg'
-declare module '*.gif'
+declare module '*.svg' {
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const content: string;
+
+  export { ReactComponent };
+  export default content;
+}
---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  webpack(config) {
+    // Support svg import as react component
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
