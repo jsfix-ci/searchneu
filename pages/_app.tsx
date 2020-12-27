706932f90376df40ebdd13c1740d4b494f7d79f5
Da-Jin Chu <dajinchu@gmail.com>
2020-12-27 01:28:26 -0500

    semantic base styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/base.scss";
 import { QueryParamProvider } from "../utils/QueryParamProvider";
+import 'semantic-ui-css/semantic.min.css';
 // TODO: Split CSS per page so we don't import all css in every page
 
 function MyApp({ Component, pageProps }) {
