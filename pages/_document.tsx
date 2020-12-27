16feb1903d919b0f9520b3c7762ed6558b83a898
Da-Jin Chu <dajinchu@gmail.com>
2020-12-27 16:07:39 -0500

    add head meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,36 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="description" content="Search for Northeastern" />
+          <meta name="author" content="Sandbox" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#ffffff" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#e75454" />
+          <link
+            rel="preload"
+            href="/icons.woff2"
+            as="font"
+            crossOrigin="anonymous"
+          />
+          <title>Search NEU</title>
           <link
             href="https://fonts.googleapis.com/css?family=Open+Sans:400,700"
             rel="stylesheet"
