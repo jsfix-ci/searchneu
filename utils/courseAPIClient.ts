fd98703be4862ee5a4d57e80e2e5738eb9daa6a2
Da-Jin Chu <dajinchu@gmail.com>
2020-12-31 23:36:06 -0500

    regen graphql sdk on yarn dev

--- utils/courseAPIClient.ts
+++ utils/courseAPIClient.ts
@@ -4,4 +4,5 @@ import { getSdk } from '../generated/graphql';
 const ENDPOINT = 'https://searchneu.com/graphql';
 
 // GraphQL codegen creates a typed SDK by pulling out all operations in *.graphql files in the project.
+// Run `yarn generate:graphql` to regenerate the client, or let `yarn dev` auto regen constantly.
 export const gqlClient = getSdk(new GraphQLClient(ENDPOINT));
