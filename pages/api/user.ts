e272087ba76fa420190b94676d5d851e91eee602
Da-Jin Chu <dajinchu@gmail.com>
2021-01-03 00:44:18 -0500

    add GET user

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,16 +1,32 @@
-import { verify } from 'jsonwebtoken';
-import { NextApiRequest, NextApiResponse } from 'next';
-import { promisify } from 'util';
+import { PrismaClient } from '@prisma/client';
+import withUser from '../../utils/api-middleware/withUser';
 
-const verifyAsync = promisify(verify);
+const prisma = new PrismaClient();
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-): Promise<void> {
-  const loginToken = req.cookies.loginToken;
-  const isTokenValid = await verifyAsync(loginToken, process.env.JWT_SECRET);
-  const userId = req.cookies.userId;
-
-  return;
+export interface GetUserResponse {
+  followedCourses: string[];
+  followedSections: string[];
 }
+
+export default withUser(
+  async (req, res): Promise<void> => {
+    const { userId } = req;
+    if (userId) {
+      if (req.method === 'GET') {
+        const user = await prisma.user.findUnique({
+          where: { id: userId },
+          include: { followedCourses: true, followedSections: true },
+        });
+        if (user) {
+          const data: GetUserResponse = {
+            followedCourses: user.followedCourses.map((c) => c.courseHash),
+            followedSections: user.followedSections.map((s) => s.sectionHash),
+          };
+          res.json(data);
+          return;
+        }
+      }
+    }
+    res.status(404).end();
+  }
+);
---
+++ utils/api-middleware/withUser.ts
@@ -0,0 +1,38 @@
+import { Secret, verify } from 'jsonwebtoken';
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+import { promisify } from 'util';
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+const verifyAsync = promisify<string, Secret, object | undefined>(verify);
+
+type NextApiRequestWithUser = NextApiRequest & { userId?: number };
+type NextApiHandlerWithUser<T = any> = (
+  req: NextApiRequestWithUser,
+  res: NextApiResponse<T>
+) => void | Promise<void>;
+
+interface AuthTokenPayload {
+  userId?: number;
+}
+
+/**
+ * Wrap a next api handler with req.userId set as the logged-in user
+ * If no authToken cookie set or cookie is invalid, userId will be undefined.
+ */
+export default function withUser<T>(
+  handler: NextApiHandlerWithUser<T>
+): NextApiHandler<T> {
+  return async (req, res) => {
+    const newReq: NextApiRequestWithUser = req;
+
+    if (req.cookies.authToken) {
+      const jwtPayload: AuthTokenPayload = await verifyAsync(
+        req.cookies.authToken,
+        process.env.JWT_SECRET
+      ).catch(); // swallow errors
+      newReq.userId = jwtPayload?.userId;
+    }
+    newReq.userId = 1;
+    return handler(newReq, res);
+  };
+}
