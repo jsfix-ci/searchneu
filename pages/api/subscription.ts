0bfdeac46e540a77b6a85932e2b319e794066dd4
NEUDitao <edward.d.li@gmail.com>
2021-01-04 11:11:26 -0800

    post/delete on users

--- pages/api/subscription.ts
+++ pages/api/subscription.ts
@@ -1,8 +1,53 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../tests/pages/api/prisma';
+import withUser from '../../utils/api-middleware/withUser';
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-): void {
-  return;
-}
+export default withUser(
+  async (req, res): Promise<void> => {
+    const { userId } = req;
+    if (!userId) {
+      res.status(401).end();
+    }
+
+    if (req.method === 'POST') {
+      if (req.body.courseHash) {
+        await prisma.followedCourse.create({
+          data: {
+            courseHash: req.body.courseHash,
+            user: { connect: { id: userId } },
+          },
+        });
+      }
+
+      if (req.body.sectionHash) {
+        await prisma.followedSection.create({
+          data: {
+            sectionHash: req.body.sectionHash,
+            user: { connect: { id: userId } },
+          },
+        });
+      }
+    }
+    if (req.method === 'DELETE') {
+      if (req.body.courseHash) {
+        await prisma.followedCourse.delete({
+          where: {
+            userId_courseHash: {
+              userId,
+              courseHash: req.body.courseHash,
+            },
+          },
+        });
+      }
+      if (req.body.sectionHash) {
+        await prisma.followedSection.delete({
+          where: {
+            userId_sectionHash: {
+              sectionHash: req.body.sectionHash,
+              userId,
+            },
+          },
+        });
+      }
+    }
+  }
+);
