fffae2a9193420ac8f504934ed5261ac89cbd0a1
Da-Jin Chu <dajinchu@gmail.com>
2021-01-09 15:48:29 -0500

    cleanup jest infrastructure

--- tests/pages/api/jest.config.js
+++ tests/pages/api/jest.config.js
@@ -2,14 +2,7 @@ module.exports = {
   name: 'dbtest',
   displayName: 'Database Tests',
   rootDir: '../../../',
-  moduleFileExtensions: ['js', 'jsx', 'json', 'node', 'tsx', 'ts'],
-  testMatch: ['**/*.{spec,test}.db.[jt]s?(x)'],
-  testPathIgnorePatterns: [
-    '<rootDir>/backend/tests/jest.config.js',
-    '<rootDir>/node_modules',
-    '<rootDir>/dist/',
-  ],
-  setupFiles: ['<rootDir>/tests/pages/api/jestSetupFile.js'],
+  testMatch: ['**/*.test.db.[jt]s?(x)'],
   setupFilesAfterEnv: ['<rootDir>/tests/pages/api/teardown.ts'],
-  testEnvironment: '<rootDir>/tests/pages/api/db_test_env.ts',
+  testEnvironment: '<rootDir>/tests/pages/api/prisma-test-env.ts',
 };
