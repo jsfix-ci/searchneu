f55a43915ce3cde9b7ef201132a4e14b09e64680
Megan Li <55663526+megandouli@users.noreply.github.com>
2021-03-25 21:54:50 -0400

    Class Pages Part 1 (#92)
    * have graphql query for classPages
    
    * set up file where class pages will live
    
    * restructured where classPages will be and added search bar
    
    * oops removed console logs
    
    * move header with search bar into own component and fix bug with invalid classIdRange filters when filters are cleared
    
    * fix bug with mobileSearchOverlay not closing, remove unwanted space in url
    
    * fix overlay bug, refactor class page path, and refactor setTermAndCampus
    
    * fix type errors, have default classIdRange filters be min 0 max 9999
    
    * make default class range filter on UI not display min 0 max 9999
    
    * update test snapshot and revert classIdRange bug back to how it was before
    
    * remove test code and update tests
    
    * import needed functions from lodash
    
    * move classPages so the searchQuery isn't on the url path
    
    * WIP: reconciling changes from Preload Pages PR
    
    * make termAndCampusToURL() flexible enough for class pages
    
    * fix tests
    
    * pass in termAndCampusToURL prop for Header
    
    * add termAndCampusToURL as dependency for termAndCampusToURLCallback
    
    Co-authored-by: puneetjohal <johal.p@northeastern.edu>
    Co-authored-by: Andrew Dai <dai.an@husky.neu.edu>

--- pages/[campus]/[termId]/search/[query].tsx
+++ pages/[campus]/[termId]/search/[query].tsx
@@ -4,17 +4,11 @@
  */
 import _ from 'lodash';
 import { GetServerSideProps } from 'next';
-import Head from 'next/head';
 import { useRouter } from 'next/router';
-import React, { ReactElement, useCallback } from 'react';
-import { BooleanParam, useQueryParam, useQueryParams } from 'use-query-params';
+import React, { ReactElement } from 'react';
+import { useQueryParams } from 'use-query-params';
 import Footer from '../../../../components/Footer';
-import {
-  getRoundedTerm,
-  getTermInfoForCampus,
-} from '../../../../components/global';
-import FilterButton from '../../../../components/icons/FilterButton.svg';
-import Logo from '../../../../components/icons/Logo';
+import Header from '../../../../components/Header';
 import macros from '../../../../components/macros';
 import EmptyResultsContainer from '../../../../components/ResultsPage/EmptyResultsContainer';
 import FeedbackModal from '../../../../components/ResultsPage/FeedbackModal/FeedbackModal';
@@ -26,44 +20,20 @@ import {
   FilterSelection,
   QUERY_PARAM_ENCODERS,
 } from '../../../../components/ResultsPage/filters';
-import MobileSearchOverlay from '../../../../components/ResultsPage/MobileSearchOverlay';
 import ResultsLoader from '../../../../components/ResultsPage/ResultsLoader';
-import SearchBar from '../../../../components/ResultsPage/SearchBar';
-import SearchDropdown from '../../../../components/ResultsPage/SearchDropdown';
-import useAtTop from '../../../../components/ResultsPage/useAtTop';
 import useSearch, {
   SearchParams,
 } from '../../../../components/ResultsPage/useSearch';
-import { Campus, EMPTY_FILTER_OPTIONS } from '../../../../components/types';
-import { campusToColor } from '../../../../utils/campusToColor';
-import Link from 'next/link';
+import { EMPTY_FILTER_OPTIONS } from '../../../../components/types';
 
 const isWindow = typeof window !== 'undefined';
 
 export default function Results(): ReactElement | null {
-  const atTop = useAtTop();
   const router = useRouter();
-  const [showOverlay, setShowOverlay] = useQueryParam('overlay', BooleanParam);
   const query = (router.query.query as string) || '';
   const termId = router.query.termId as string;
-  const campus = router.query.campus as string;
 
   const [qParams, setQParams] = useQueryParams(QUERY_PARAM_ENCODERS);
-
-  const setSearchQuery = (q: string): void => {
-    router.push(
-      `/${campus}/${termId}/search/${encodeURIComponent(q)}${
-        window.location.search
-      }`
-    );
-  };
-  const termAndCampusToURL = useCallback(
-    (t: string, newCampus: string) =>
-      `/${newCampus}/${t}/search/${encodeURIComponent(query)}${
-        isWindow && window.location.search
-      }`,
-    [query]
-  );
 
   const filters: FilterSelection = _.merge(
     {},
@@ -85,101 +55,25 @@ export default function Results(): ReactElement | null {
 
   const filtersAreSet: boolean = areFiltersSet(filters);
 
-  if (showOverlay && macros.isMobile) {
-    return (
-      <MobileSearchOverlay
-        query={query}
-        filterSelection={filters}
-        filterOptions={searchData?.filterOptions || EMPTY_FILTER_OPTIONS()}
-        setFilterPills={setQParams}
-        setQuery={(q: string) => setSearchQuery(q)}
-        onExecute={() => setShowOverlay(false)}
-      />
-    );
-  }
+  const termAndCampusToURL = (
+    t: string,
+    newCampus: string,
+    query: string
+  ): string => {
+    return `/${newCampus}/${t}/search/${encodeURIComponent(query)}${
+      isWindow && window.location.search
+    }`;
+  };
 
   return (
     <div>
-      <Head>
-        <title>Search NEU - {query}</title>
-      </Head>
-      <div className={`Results_Header ${atTop ? 'Results_Header-top' : ''}`}>
-        <Link href={`/${campus}/${termId}`}>
-          <a className="Results__Logo--wrapper">
-            <Logo
-              className="Results__Logo"
-              aria-label="logo"
-              campus={campus as Campus}
-            />
-          </a>
-        </Link>
-        <div className="Results__spacer" />
-        {macros.isMobile && (
-          <div className="Results__mobileSearchFilterWrapper">
-            <div className="Results__searchwrapper">
-              <SearchBar
-                onSearch={setSearchQuery}
-                query={query}
-                buttonColor={campusToColor[campus]}
-              />
-            </div>
-            <FilterButton
-              className="Results__filterButton"
-              aria-label="filter-button"
-              onClick={() => {
-                if (macros.isMobile) {
-                  setShowOverlay(true);
-                }
-              }}
-            />
-          </div>
-        )}
-        {!macros.isMobile && (
-          <div className="Results__searchwrapper">
-            <SearchBar
-              onSearch={setSearchQuery}
-              query={query}
-              buttonColor={campusToColor[campus]}
-            />
-          </div>
-        )}
-        <div className="Breadcrumb_Container">
-          <div className="Breadcrumb_Container__dropDownContainer">
-            <SearchDropdown
-              options={Object.keys(Campus).map((c: Campus) => ({
-                text: c,
-                value: c,
-                link: termAndCampusToURL(getRoundedTerm(c, termId), c),
-              }))}
-              value={campus}
-              className="searchDropdown"
-              compact={false}
-            />
-          </div>
-          <span className="Breadcrumb_Container__slash">/</span>
-          <div className="Breadcrumb_Container__dropDownContainer">
-            <SearchDropdown
-              options={getTermInfoForCampus(Campus[campus.toUpperCase()]).map(
-                (terminfo) => ({
-                  text: terminfo.text,
-                  value: terminfo.value,
-                  link: termAndCampusToURL(terminfo.value, campus),
-                })
-              )}
-              value={termId}
-              className="searchDropdown"
-              compact={false}
-              key={campus}
-            />
-          </div>
-        </div>
-        {/*<Icon
-          name="setting"
-          size="large"
-          className="Notifications_Settings"
-          onClick={() => router.push('/notifications_settings')}
-        />*/}
-      </div>
+      <Header
+        router={router}
+        title={`Search NEU - ${query}`}
+        searchData={searchData}
+        termAndCampusToURL={termAndCampusToURL}
+      ></Header>
+
       {!macros.isMobile && <FeedbackModal />}
       <div className="Results_Container">
         {!macros.isMobile && (
