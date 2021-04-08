17e0b45ff32d0bb9267b8ea68b35b09893598a78
NEUDitao <edward.d.li@gmail.com>
2021-04-07 22:37:24 -0400

    Revert "Ad 404 page (#90)"
    This reverts commit 506301cbfa08e8b238c6b189f65cb84e298d5318.

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,5 @@
 import { ReactElement } from 'react';
-import Link from 'next/link';
-import Four04 from '../components/icons/404.svg';
 
 export default function NotFoundPage(): ReactElement {
-  return (
-    <div className="four04-container">
-      <div className="four04-title-text">something's borked</div>
-      <Four04 />
-      <Link href="/">
-        <div className="four04-return-button">BACK TO HOME</div>
-      </Link>
-    </div>
-  );
+  return <h1>404: YOU GOT LIGMA</h1>;
 }
