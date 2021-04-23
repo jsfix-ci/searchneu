73f3cbdc6313f3c7420529337f4348a63c04b8a7
Amiel Monasterial <amielmon@gmail.com>
2021-04-22 21:00:18 -0400

    env variable

--- pages/api/feedback.ts
+++ pages/api/feedback.ts
@@ -16,7 +16,7 @@ const post: NextApiHandler = async (
   const { text } = req.body;
   console.log('text', text);
   const slackRes = await axios.post(
-    'https://hooks.slack.com/services/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',
+    process.env.SLACK_SUPPORT_ENDPOINT,
     req.body
   );
   console.log('slackRes', slackRes);
