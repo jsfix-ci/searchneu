6d312d11375406a455511ecfa70e10b87e6c67c7
Simar Chadha <simarchadha1@gmail.com>
2021-12-07 13:53:46 -0500

    Filter doesn't show on NEU page. Current way works, but could be done better stylistically.

--- components/ResultsPage/FilterPanel.tsx
+++ components/ResultsPage/FilterPanel.tsx
@@ -4,6 +4,7 @@ import CheckboxFilter from './CheckboxFilter';
 import DropdownFilter from './DropdownFilter';
 import { FilterOptions, FilterSelection, FILTERS_IN_ORDER } from './filters';
 import RangeFilter from './RangeFilter';
+import { useRouter } from 'next/router';
 
 export interface FilterPanelProps {
   options: FilterOptions;
@@ -16,21 +17,25 @@ function FilterPanel({
   selected,
   setActive,
 }: FilterPanelProps): ReactElement {
+  const router = useRouter();
+  const campus = router.query.campus as string;
   return (
     <div className="FilterPanel">
       {FILTERS_IN_ORDER.map(({ key, display, category }, index) => {
         const aFilter = selected[key];
         const setActiveFilter = (a): void => setActive({ [key]: a });
+        console.log(`Campus: ${campus}, Display: ${display}`);
         return (
           <React.Fragment key={index}>
-            {category === 'Dropdown' && (
-              <DropdownFilter
-                title={display}
-                options={options[key]}
-                selected={aFilter}
-                setActive={setActiveFilter}
-              />
-            )}
+            {category === 'Dropdown' &&
+              (campus !== 'NEU' || display !== 'Term Half') && (
+                <DropdownFilter
+                  title={display}
+                  options={options[key]}
+                  selected={aFilter}
+                  setActive={setActiveFilter}
+                />
+              )}
             {category === 'Checkboxes' && (
               <CheckboxFilter
                 title={display}
