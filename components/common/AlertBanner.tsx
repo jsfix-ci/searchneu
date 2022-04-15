c6667172b3f937e098c6a25376c5dd1e49c4ef40
sebwittr <79394243+sebwittr@users.noreply.github.com>
2022-04-15 15:01:08 -0400

    Sw mobile home page bug (#151)
    * Adding code to fix bugs on mobile homepage
    
    * Reverted test files
    
    * changed styling
    
    Co-authored-by: Sebastian Wittrock <wittrock.s@northeastern.edu>

--- components/common/AlertBanner.tsx
+++ components/common/AlertBanner.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import macros from '../macros';
 import IconClose from '../icons/IconClose';
 
 type AlertLevel = 'error' | 'warning' | 'info';
@@ -18,20 +19,29 @@ export default function AlertBanner({ alertBannerData }: AlertBannerProps) {
 
   return (
     isVisible && (
-      <div className={`alertBanner ${alertBannerData.alertLevel}Banner`}>
-        <span>
-          {alertBannerData.text}
-          {alertBannerData.link && (
-            <a href={alertBannerData.link}> Learn More.</a>
-          )}
-        </span>
-        <div
-          className="alertBanner__back"
-          role="button"
-          tabIndex={0}
-          onClick={() => setIsVisible(false)}
-        >
-          <IconClose fill="#808080" />
+      //if on mobile, it'll put the text and button into columns.
+      <div
+        className={
+          macros.isMobile ? 'alertBanner__parent' : 'alertBanner__mobileParent'
+        }
+      >
+        <div className={`alertBanner ${alertBannerData.alertLevel}Banner`}>
+          <div className="alertBanner__text">
+            <span>
+              {alertBannerData.text}
+              {alertBannerData.link && (
+                <a href={alertBannerData.link}> Learn More.</a>
+              )}
+            </span>
+          </div>
+          <div
+            className="alertBanner__back"
+            role="button"
+            tabIndex={0}
+            onClick={() => setIsVisible(false)}
+          >
+            <IconClose fill="#808080" />
+          </div>
         </div>
       </div>
     )
